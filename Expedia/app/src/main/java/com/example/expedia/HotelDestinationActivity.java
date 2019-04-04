5f3c8f0ca672d2d7048bf49ee067c2dc10abef6b
kjy <wodusl1103@naver.com>
2019-04-04 17:30:39 +0900

    호텔 검색 세부 창(수정중)
    목적지 검색창, 날짜 선택창(다이얼로그 사용, 기간 선택으로 추후 수정)

---
+++ Expedia/app/src/main/java/com/example/expedia/HotelDestinationActivity.java
@@ -0,0 +1,66 @@
+package com.example.expedia;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class HotelDestinationActivity extends AppCompatActivity {
+    private boolean activityStartup = true;
+    SearchView searchView;
+    ImageView back;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel_destination);
+
+       searchView = findViewById(R.id.searchView);
+       back = findViewById(R.id.cancel_btn);
+       back.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               finish();
+           }
+       });
+
+        // SearchView 입력 글자색과 힌트 색상 변경하기
+        SearchView.SearchAutoComplete searchAutoComplete = searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchAutoComplete.setHintTextColor(Color.GRAY);
+        searchAutoComplete.setTextColor(Color.BLACK);
+
+        // Inside onCreate
+
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    if (activityStartup) {
+                        searchView.clearFocus();
+                        activityStartup = false;
+                    }
+                }
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Toast.makeText(HotelDestinationActivity.this, "[검색버튼클릭] 검색어 = "+query, Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Toast.makeText(HotelDestinationActivity.this, "입력하고있는 단어 = "+newText, Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+    }
+}
--- Expedia/app/src/main/java/com/example/expedia/HotelSearchActivity.java
+++ Expedia/app/src/main/java/com/example/expedia/HotelSearchActivity.java
@@ -1,12 +1,19 @@
 package com.example.expedia;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class HotelSearchActivity extends AppCompatActivity {
     ImageView cancelImage;
@@ -27,8 +34,34 @@ public class HotelSearchActivity extends AppCompatActivity {
         });
 
         destination = findViewById(R.id.destination_text);
+        destination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HotelSearchActivity.this, HotelDestinationActivity.class));
+            }
+        });
         date = findViewById(R.id.date_text);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // DatePickerDialog
+                Calendar calendar = Calendar.getInstance();
+                DatePickerDialog dialog = new DatePickerDialog(HotelSearchActivity.this, listener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+                long now = System.currentTimeMillis();
+                dialog.getDatePicker().setMinDate(now);
+                dialog.show();
+            }
+        });
         person = findViewById(R.id.person_text);
         searchbtn = findViewById(R.id.search_button);
+        startActivity(new Intent(HotelSearchActivity.this, HotelDestinationActivity.class));
     }
+
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            monthOfYear += 1;
+            Toast.makeText(getApplicationContext(), year + "년" + monthOfYear + "월" + dayOfMonth +"일", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
